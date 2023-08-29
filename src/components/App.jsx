8696ab3504070c9e81bc5413b87df53359cfeea2
andriy <evicandrij@gmail.com>
2023-08-29 11:50:07 +0300

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 import Section from './Section/Section';
-import './App.css';
 
 class App extends Component {
   render() {
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,15 +1,15 @@
-import './FeedbackOptions.css';
+import css from  './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <>
-      <div className="button-conteiner">
+      <div className={css.button_conteiner}>
         {options.map(label => {
           return (
             <button
-              className="Feedback_button"
+              className={css.Feedback_button}
               key={shortid.generate()}
               onClick={() => onLeaveFeedback(label)}
             >
